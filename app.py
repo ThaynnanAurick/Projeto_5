63527756d2a1af11f7fc7a7cd80a7648cc0a3340
ThaynnanAurick <thaynnanaurick@gmail.com>
2024-10-24 11:56:56 +0100

    Ajustado repositorio

---
+++ app.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import plotly.express as px
+import streamlit as st
+
+st.header("Análise de Veículos Usados")
+car_data = pd.read_csv(r"C:\Users\Thaynna\Desktop\Projeto 5\Noteboo\vehicles.csv.csv")
+hist_button = st.button("Criar histograma de Veículos usados")
+
+if hist_button:
+    st.white("Criando um histograma para o conjunto de dados de anúncio de veiculos de vendas de carros")
+
+    fig = px.histogram(car_data, x="odometer")
+
+    st.plotly.chart(fig, user_container_width=True)
