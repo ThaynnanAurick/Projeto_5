bf29f92cb2719ec6b95008ffa813337cb7542506
ThaynnanAurick <thaynnanaurick@gmail.com>
2024-10-24 14:10:24 +0100

    grafico de disperção

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header("Análise de Veículos Usados")
+st.header("Análises de Veículos Usados")
 car_data = pd.read_csv("vehicles.csv.csv")
 hist_button = st.button("Criar histograma de Veículos usados")
 
@@ -12,4 +12,12 @@ if hist_button:
     fig = px.histogram(car_data, x="odometer")
 
     st.plotly_chart(fig, user_container_width=True)
+
+
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv("vehicles.csv.csv") # lendo os dados
+fig = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
+fig.show() # exibindo
    
